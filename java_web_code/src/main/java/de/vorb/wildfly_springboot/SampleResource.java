4a8aea049284736825915e68487b8b57c7ce28aa
ranjith5076457 <52430351+ranjith5076457@users.noreply.github.com>
2021-04-23 07:47:01 +0530

    Update SampleResource.java

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -8,7 +8,7 @@ public class SampleResource {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Hello World! im an devops engineer";
     }
 
 }
